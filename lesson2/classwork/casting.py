76a5aad7abf5426920b7118e5cb9d3e938d8de85
Lowel-Smachwell <godforone10@gmail.com>
2026-07-09 20:30:42 +0000

    math operator

--- lesson2/classwork/casting.py
+++ lesson2/classwork/casting.py
@@ -1,7 +1,7 @@
 num_str = "42"
 # "42" -> 42
-# num_value + int(num_str)
-print(num_str + "1")
+num_value = int(num_str)
+print(num_value + 1)
 
 decimal_str = "3.14"
 # "3.14" -> 3.14
@@ -9,11 +9,11 @@ decimal_value = float(decimal_str)
 print(decimal_value + 2.19)
 
 # "16" not 16
-age_text = input("How old are you ")
+age_text = input("How old are you? ")
 age = int(age_text)
-print('Next year you will be', age + 1, "years old.")
+print("Next year you will be", age + 1, "years old.")
 
-num = 100
+num = 20
 # 100 -> "100"
 num_str2 = str(num)
-print("This burger cost $" + num_str2)
+print("This burger costs $" + num_str2)
---
+++ lesson2/classwork/math_operators.py
@@ -0,0 +1,21 @@
+print(4 + 5) #Addition
+
+print(10 - 3) #Subtraction
+
+print(6 * 7) # Multiplication
+
+print(15 / 2) # Division
+
+print(15 // 2) # floor division: rounding down basically
+
+print(15 % 2) # Modulus: gives remainder
+
+print(2 ** 3) # Exponentitation: first number raised to the second number
+
+print(2 + 3 * 4) # 14 (3 * 4 = 12, then 2 +12 =14)
+print((2 + 3) * 4) # 20 (2 + 3 = 5, then 5 * 4 = 20)
+
+a = 8
+b = 3
+print("a + b =", a + b)
+print("a * b")
